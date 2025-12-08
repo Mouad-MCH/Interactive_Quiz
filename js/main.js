642808abea7addb56155021afd34cdf3537683a6
hibaHAJAJI <wiamhiba08@gmail.com>
2025-12-08 16:26:14 +0100

    add quize file from hiba

--- js/main.js
+++ js/main.js
@@ -117,6 +117,10 @@ const take_Quiz_btn = document.getElementById("btn_take");
 const list = document.getElementById("list");
 
 
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-button");
+const nextButton = document.getElementById("next");
+
 
 
 
@@ -125,6 +129,8 @@ const list = document.getElementById("list");
  * - Variables
 \* --------------------- */
 
+let currentQuestionIndex =0;
+let score =0;
 
 
 
@@ -135,7 +141,7 @@ const list = document.getElementById("list");
 
 /* ************* Take button *********************** */
 
-take_Quiz_btn.addEventListener('click', () => {
+function take() {
     take_Quiz_btn.classList.toggle('active')
 
     if(take_Quiz_btn.classList.contains('active')) {
@@ -143,10 +149,63 @@ take_Quiz_btn.addEventListener('click', () => {
     }else {
         list.classList.remove("show")
     }
-    
-})
+}
 
 /*  **********************************************  */
 
 
 
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    showQuestion(questions_Html);
+}
+
+
+function showQuestion(questions){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML=questionNo +"."+ currentQuestion.question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+           button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    })
+}
+
+
+function resetState(){
+    nextButton.style.display ="none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+function selectAnswer(e){
+    const selectedBtn= e.target;
+    const isCorrect = selectedBtn.dataset.correct==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct ==="true"){
+          button.classList.add("corr;ect")
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display="block";
+}
+
+
+startQuiz();
